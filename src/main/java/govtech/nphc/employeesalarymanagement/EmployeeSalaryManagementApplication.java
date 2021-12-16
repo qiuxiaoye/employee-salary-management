d5945373b46f0a74c30f070f8e4b5dd56ab16223
Xiaoye Qiu <qiuxiaoye@hotmail.com>
2021-12-16 22:30:23 +0800

    Initial commit

---
+++ src/main/java/govtech/nphc/employeesalarymanagement/EmployeeSalaryManagementApplication.java
@@ -0,0 +1,13 @@
+package govtech.nphc.employeesalarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSalaryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSalaryManagementApplication.class, args);
+    }
+
+}
---
+++ src/main/java/govtech/nphc/employeesalarymanagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package govtech.nphc.employeesalarymanagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeSalaryManagementApplication.class);
+    }
+
+}
