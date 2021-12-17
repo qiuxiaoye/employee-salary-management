4b04f9c8dd2d33947b2805d50e4d24aebaf03e29
Xiaoye Qiu <qiuxiaoye@hotmail.com>
2021-12-17 20:20:10 +0800

    add csv exception

--- src/main/java/govtech/nphc/employeesalarymanagement/service/CSVService.java
+++ src/main/java/govtech/nphc/employeesalarymanagement/service/CSVService.java
@@ -1,4 +1,30 @@
 package govtech.nphc.employeesalarymanagement.service;
 
+import govtech.nphc.employeesalarymanagement.helper.CSVHelper;
+import govtech.nphc.employeesalarymanagement.model.Employee;
+import govtech.nphc.employeesalarymanagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
 public class CSVService {
+    @Autowired
+    EmployeeRepository repository;
+
+    public void save(MultipartFile file) {
+        try {
+            List<Employee> tutorials = CSVHelper.csvToTutorials(file.getInputStream());
+            repository.saveAll(tutorials);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+
+    public List<Employee> getAllTutorials() {
+        return repository.findAll();
+    }
 }
